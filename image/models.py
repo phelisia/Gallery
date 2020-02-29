de3dc7382cf1bd43efb9e430b21ea2d7539dd30e
phelisia <phelisiajeruto@gmail.com>
2020-02-29 18:36:27 +0300

    added location in my image app

--- image/models.py
+++ image/models.py
@@ -6,3 +6,25 @@ import datetime as dt
 
 class Category(models.Model):
     name = models.CharField(max_length =50)
+
+    def __str__(self):
+        return self.name
+
+    def save_category(self):
+        self.save()
+
+    def delete_category(self):
+        self.delete()
+
+class Location(models.Model):
+    name = models.CharField(max_length =70)
+    @classmethod
+    def get_locations(cls):
+         location = Location.objects.all()
+         return locations
+    def __str__(self):
+        return self.name
+
+
+
+
