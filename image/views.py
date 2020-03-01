8ca018f2ef481ec0c3970b3056f4c5b817454892
phelisia <phelisiajeruto@gmail.com>
2020-03-01 17:43:03 +0300

    added search tml

--- image/views.py
+++ image/views.py
@@ -42,3 +42,14 @@ def past_days_image(request, past_date):
         return redirect(image_of_day)
 
     return render(request, 'photo/past-image.html', {"date": date})
+    
+def search_results(request):
+    if 'imagesearch' in request.GET and request.GET["imagesearch"]:
+        category = request.GET.get("imagesearch")
+        searched_images = Image.search_by_category(category)
+        message = f"{category}"
+        print(searched_images)
+        return render(request, 'photo/search_results.html', {"message": message, "images": searched_images})
+    else:
+        message = "You haven't searched for any image category"
+        return render(request, 'photo/search_results.html', {"message": message})
