15e0adc0fa5806f6da6aaef4e48e4f2f2845fb3b
phelisia <phelisiajeruto@gmail.com>
2020-03-01 23:36:20 +0300

    added project title in my readme file

--- image/urls.py
+++ image/urls.py
@@ -3,7 +3,7 @@ from . import views
 
 urlpatterns=[
     
-    url(r'^$',views.image_of_day,name='imageToday'),
-    url(r'^archives/(\d{4}-\d{2}-\d{2})/$',views.past_days_image,name = 'pastImage'),
+     url(r'^location/', views.image_location, name='location'),
+    # url(r'^archives/(\d{4}-\d{2}-\d{2})/$',views.past_days_image,name = 'pastImage'),
     url(r'^search/', views.search_results, name='search'),
 ]
--- image/views.py
+++ image/views.py
@@ -1,48 +1,21 @@
 from django.shortcuts import render
-from django.http  import HttpResponse
-import datetime as dt
-from django.http  import HttpResponse,Http404
+from .models import Image,Location
 
 # Create your views here.
-def welcome(request):
-    return render(request, 'welcome.html')
+def index(request):
+    images = Image.objects.all()
+    locations = Location.get_locations()
+    print(locations)
+    return render(request, 'photo/index.html', {'images': images[::-1], 'locations': locations})
+
     
 
-def image_of_day(request):
-    date = dt.date.today()
-    return render(request, 'photo/today-image.html', {"date": date,})
+def image_location(request, location):
+    images = Image.filter_by_location(location)
+    print(images)
+    return render(request, 'photo/location.html', {'location_images': images})
 
-    # FUNCTION TO CONVERT DATE OBJECT TO FIND EXACT DAY
-    day = convert_dates(date)
-    return HttpResponse(html)
 
-def convert_dates(dates):
-
-    # Function that gets the weekday number for the date.
-    day_number = dt.date.weekday(dates)
-
-    days = ['Monday','Tuesday','Wednesday','Thursday','Friday','Saturday',"Sunday"]
-
-    # Returning the actual day of the week
-    day = days[day_number]
-    return day
-
-def past_days_image(request, past_date):
-
-    try:
-        # Converts data from the string Url
-        date = dt.datetime.strptime(past_date, '%Y-%m-%d').date()
-
-    except ValueError:
-        # Raise 404 error when ValueError is thrown
-        raise Http404()
-        assert False
-
-    if date == dt.date.today():
-        return redirect(image_of_day)
-
-    return render(request, 'photo/past-image.html', {"date": date})
-    
 def search_results(request):
     if 'imagesearch' in request.GET and request.GET["imagesearch"]:
         category = request.GET.get("imagesearch")
