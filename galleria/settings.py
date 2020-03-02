252017480835210c89ca435b84df706b207dc02e
phelisia <phelisiajeruto@gmail.com>
2020-03-02 11:59:09 +0300

    edited database settings

--- galleria/settings.py
+++ galleria/settings.py
@@ -83,11 +83,12 @@ DATABASES = {
         'ENGINE': 'django.db.backends.postgresql',
         'NAME': 'gallery',
         'USER': 'moringaschool',
-    'PASSWORD':'Access',
+        'PASSWORD':'Access',
     }
 }
 
-
+db_from_env = dj_database_url.config(conn_max_age=500)
+DATABASES['default'].update(db_from_env)
 # Password validation
 # https://docs.djangoproject.com/en/1.11/ref/settings/#auth-password-validators
 
